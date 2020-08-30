fb2b63cf608d89cacfcde312ea460fb258c9110b
Gravemind2401 <inferi_sententia@hotmail.com>
2020-08-30 13:11:33 +1000

    merge MiniMetaViewer into PropertyView

--- Reclaimer.DXViewer/Controls/PropertyView.xaml.cs
+++ Reclaimer.DXViewer/Controls/PropertyView.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Reclaimer.Models;
+﻿using Adjutant.Geometry;
+using Reclaimer.Models;
+using Reclaimer.Plugins.MetaViewer;
 using Reclaimer.Plugins.MetaViewer.Halo3;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Reclaimer.Controls
 {
@@ -22,16 +26,40 @@ namespace Reclaimer.Controls
     /// </summary>
     public partial class PropertyView : IScenarioPropertyView
     {
+        #region Dependency Properties
+        public static readonly DependencyProperty ShowInvisiblesProperty =
+            DependencyProperty.Register(nameof(ShowInvisibles), typeof(bool), typeof(PropertyView), new PropertyMetadata(false, ShowInvisiblesChanged));
+
+        public bool ShowInvisibles
+        {
+            get { return (bool)GetValue(ShowInvisiblesProperty); }
+            set { SetValue(ShowInvisiblesProperty, value); }
+        }
+
+        public static void ShowInvisiblesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //MetaViewerPlugin.Settings.ShowInvisibles = e.NewValue as bool? ?? false;
+        }
+        #endregion
+
+        private readonly Dictionary<string, MetaValueBase> valuesById;
+
         private ScenarioModel scenario;
         private MetaContext context;
+        private XmlNode rootNode;
+        private long baseAddress;
 
         public TabModel TabModel { get; }
         public ObjectPlacement CurrentItem { get; private set; }
+        public ObservableCollection<MetaValueBase> Metadata { get; }
 
         public PropertyView()
         {
             InitializeComponent();
+            valuesById = new Dictionary<string, MetaValueBase>();
+            Metadata = new ObservableCollection<MetaValueBase>();
             TabModel = new TabModel(this, Studio.Controls.TabItemType.Tool) { Header = "Properties" };
+            DataContext = this;
         }
 
         public void ClearScenario()
@@ -48,26 +76,49 @@ namespace Reclaimer.Controls
 
         public void ClearProperties()
         {
-            metaViewer.Metadata.Clear();
-            metaViewer.Visibility = Visibility.Hidden;
+            Metadata.Clear();
+            Visibility = Visibility.Hidden;
         }
 
-        public void SetValue(string id, object value) => metaViewer.SetValue(id, value);
+        public void SetValue(string id, object value)
+        {
+            if (!valuesById.ContainsKey(id))
+                return;
+
+            var meta = valuesById[id];
+
+            var single = meta as SimpleValue;
+            if (single != null)
+            {
+                single.Value = value;
+                return;
+            }
+
+            var multi = meta as MultiValue;
+            if (multi != null)
+            {
+                var vector = (IXMVector)value;
+                multi.Value1 = vector.X;
+                multi.Value2 = vector.Y;
+                multi.Value3 = vector.Z;
+                multi.Value4 = vector.W;
+            }
+        }
 
         public void ShowProperties(NodeType nodeType, int itemIndex)
         {
-            metaViewer.Metadata.Clear();
-            metaViewer.Visibility = Visibility.Hidden;
+            Visibility = Visibility.Hidden;
 
             var paletteKey = PaletteType.FromNodeType(nodeType);
             if (paletteKey != null && itemIndex >= 0)
             {
                 var palette = scenario.Palettes[paletteKey];
-                var baseAddress = palette.PlacementBlockRef.TagBlock.Pointer.Address
+                rootNode = palette.PlacementsNode;
+                baseAddress = palette.PlacementBlockRef.TagBlock.Pointer.Address
                     + itemIndex * palette.PlacementBlockRef.BlockSize;
 
-                metaViewer.LoadMetadata(context, palette.PlacementsNode, baseAddress);
                 CurrentItem = palette.Placements[itemIndex];
+                LoadData();
             }
             else
             {
@@ -76,7 +127,9 @@ namespace Reclaimer.Controls
                 switch (nodeType)
                 {
                     case NodeType.Mission:
-                        metaViewer.LoadMetadata(context, scenario.Sections["mission"].Node, scenario.ScenarioTag.MetaPointer.Address);
+                        rootNode = scenario.Sections["mission"].Node;
+                        baseAddress = scenario.ScenarioTag.MetaPointer.Address;
+                        LoadData();
                         break;
 
                     default:
@@ -84,7 +137,48 @@ namespace Reclaimer.Controls
                 }
             }
 
-            metaViewer.Visibility = Visibility.Visible;
+            Visibility = Visibility.Visible;
+        }
+
+        private void LoadData()
+        {
+            Metadata.Clear();
+            valuesById.Clear();
+            foreach (XmlNode n in rootNode.ChildNodes)
+            {
+                try
+                {
+                    var meta = MetaValueBase.GetMetaValue(n, context, baseAddress);
+                    Metadata.Add(meta);
+                    if (n.Attributes["id"] != null)
+                        valuesById.Add(n.Attributes["id"].Value, meta);
+                }
+                catch { }
+            }
+        }
+
+        private void RecursiveToggle(IEnumerable<MetaValueBase> collection, bool value)
+        {
+            foreach (var s in collection.OfType<IExpandable>())
+            {
+                s.IsExpanded = value;
+                RecursiveToggle(s.Children, value);
+            }
+        }
+
+        private void btnReload_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnCollapseAll_Click(object sender, RoutedEventArgs e)
+        {
+            RecursiveToggle(Metadata, false);
+        }
+
+        private void btnExpandAll_Click(object sender, RoutedEventArgs e)
+        {
+            RecursiveToggle(Metadata, true);
         }
     }
 }
